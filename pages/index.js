413a3842223ae4561ed93706f1689922faf3e070
aaronpurewal <68361537+aaronpurewal@users.noreply.github.com>
2023-05-26 10:34:11 -0700

    vercel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import Replicate from "replicate";
+
+
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
